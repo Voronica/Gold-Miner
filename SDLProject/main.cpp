24cef27a51f3fec8c893c0706e4fb4a75578f796
Kewei Zhang <jeffzhkw@gmail.com>
2020-07-30 15:09:40 -0400

    init project, add files with minor cleanup

--- SDLProject/main.cpp
+++ SDLProject/main.cpp
@@ -7,32 +7,136 @@
 #define GL_GLEXT_PROTOTYPES 1
 #include <SDL.h>
 #include <SDL_opengl.h>
+#include "glm/mat4x4.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "ShaderProgram.h"
+
+//include other header files here
 
 SDL_Window* displayWindow;
 bool gameIsRunning = true;
 
-int main(int argc, char* argv[]) {
+ShaderProgram program;
+glm::mat4 modelMatrix, viewMatrix, projectionMatrix;
+
+//declare other global var here
+
+void Initialize(){
     SDL_Init(SDL_INIT_VIDEO);
-    displayWindow = SDL_CreateWindow("Hello, World!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL);
+    displayWindow = SDL_CreateWindow("Hello, World!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 960, SDL_WINDOW_OPENGL);
     SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
     SDL_GL_MakeCurrent(displayWindow, context);
-    
+        
 #ifdef _WINDOWS
     glewInit();
 #endif
     
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    program.Load("shaders/vertex_textured.glsl", "shaders/fragment_textured.glsl");
     
+    viewMatrix = glm::mat4(1.0f);
+    modelMatrix = glm::mat4(1.0f);
+    projectionMatrix = glm::ortho(-20.0f, 20.0f, -15.0f, 15.0f, -1.0f, 1.0f);
+    
+    program.SetProjectionMatrix(projectionMatrix);
+    program.SetViewMatrix(viewMatrix);
+    glUseProgram(program.programID);
+    
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    
+    //init game objects here
+    
+}
+
+void ProcessInput(){
     SDL_Event event;
-    while (gameIsRunning) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
+    while (SDL_PollEvent(&event)) {
+        switch (event.type) {
+            case SDL_QUIT:
+            case SDL_WINDOWEVENT_CLOSE:
                 gameIsRunning = false;
-            }
+                break;
+                
+            case SDL_KEYDOWN:
+                switch (event.key.keysym.sym) {
+                    case SDLK_LEFT:
+                        // Move the player left
+                        break;
+                        
+                    case SDLK_RIGHT:
+                        // Move the player right
+                        break;
+                        
+                    case SDLK_SPACE:
+                        // Some sort of action
+                        break;
+                }
+                break; // SDL_KEYDOWNULL
         }
+    }
+    
+    const Uint8 *keys = SDL_GetKeyboardState(NULL);
+    
+    if (keys[SDL_SCANCODE_LEFT]) {
         
-        glClear(GL_COLOR_BUFFER_BIT);
-        SDL_GL_SwapWindow(displayWindow);
+    }
+    else if (keys[SDL_SCANCODE_RIGHT]) {
+        
+    }
+    
+    //Do normalize here
+//    if (glm::length(currentScene->state.player->movement) > 1.0f) {
+//        currentScene->state.player->movement = glm::normalize(currentScene->state.player->movement);
+//    }
+    
+    
+}
+
+#define FIXED_TIMESTEP 0.0166666f
+float lastTicks = 0;
+float accumulator = 0.0f;
+
+void Update(){
+    float ticks = (float)SDL_GetTicks() / 1000.0f;
+    float deltaTime = ticks - lastTicks;
+    lastTicks = ticks;
+    deltaTime += accumulator;
+    if (deltaTime < FIXED_TIMESTEP) {
+        accumulator = deltaTime;
+        return;
+    }
+    
+    while (deltaTime >= FIXED_TIMESTEP) {
+        // Update. Notice it's FIXED_TIMESTEP. Not deltaTime
+        
+        deltaTime -= FIXED_TIMESTEP;
+        
+    }
+    accumulator = deltaTime;
+    //Update viewMatrix?
+    
+}
+
+void Render(){
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    
+    SDL_GL_SwapWindow(displayWindow);
+    
+}
+int main(int argc, char* argv[]) {
+    
+    Initialize();
+    
+    while (gameIsRunning) {
+        
+        ProcessInput();
+        Update();
+        
+        //Scene switch here
+        Render();
+        
     }
     
     SDL_Quit();
